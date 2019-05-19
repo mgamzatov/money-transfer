6e4b7783ee9532e164764a975cf6dede3c18e777
mgamzatov <gamz360@gmail.com>
2019-05-19 22:36:48 +0300

    add validation for transfer

--- src/main/kotlin/ru/magomed/gamzatov/money/transfer/application/service/TransferService.kt
+++ src/main/kotlin/ru/magomed/gamzatov/money/transfer/application/service/TransferService.kt
@@ -2,10 +2,12 @@ package ru.magomed.gamzatov.money.transfer.application.service
 
 import ru.magomed.gamzatov.money.transfer.application.exception.BadRequestException
 import ru.magomed.gamzatov.money.transfer.application.model.Transfer
+import java.math.BigDecimal
 
 object TransferService {
 
     fun transfer(transfer: Transfer) {
+        validate(transfer)
         val from = AccountService.getById(transfer.fromAccountId)
         val to = AccountService.getById(transfer.toAccountId)
         val amount = transfer.amount
@@ -23,6 +25,17 @@ object TransferService {
                 }
                 from.money -= amount
                 to.money += amount
+            }
+        }
+    }
+
+    private fun validate(transfer: Transfer) {
+        with(transfer) {
+            if (fromAccountId == toAccountId) {
+                throw BadRequestException("fromAccountId and toAccountId must be different")
+            }
+            if (amount <= BigDecimal.ZERO) {
+                throw BadRequestException("Amount must be bigger than zero")
             }
         }
     }
